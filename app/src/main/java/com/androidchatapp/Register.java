49f1fffddd55df02d2894701f75f41a956d3b82e
ilian001 <Ivan@ivans-air.home>
2018-05-02 22:51:21 -0700

    add image

--- app/src/main/java/com/androidchatapp/Register.java
+++ app/src/main/java/com/androidchatapp/Register.java
@@ -90,6 +90,7 @@ public class Register extends AppCompatActivity {
 
                                                 if (!obj.has(user)) {
                                                     reference.child(user).child("password").setValue(pass);
+                                                    reference.child(user).child("image").setValue(img);
                                                     Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
                                                 } else {
                                                     Toast.makeText(Register.this, "username already exists", Toast.LENGTH_LONG).show();
