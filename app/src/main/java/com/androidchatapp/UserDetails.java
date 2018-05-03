71028a3d2dd5698837d74b989967870547019841
parthmangrola <parthmangrola@gmail.com>
2018-05-03 00:06:03 -0700

    revert back

--- app/src/main/java/com/androidchatapp/UserDetails.java
+++ app/src/main/java/com/androidchatapp/UserDetails.java
@@ -1,16 +1,7 @@
 package com.androidchatapp;
 
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
-
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-
 public class UserDetails {
     static String username = "";
     static String password = "";
     static String chatWith = "";
-    static BufferedImage image;
 }
