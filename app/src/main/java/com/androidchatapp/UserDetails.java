5a4df30902307b68c0504c3359b38d15ce931c94
ilian001 <Ivan@ivans-air.home>
2018-05-02 22:17:45 -0700

    image variable

--- app/src/main/java/com/androidchatapp/UserDetails.java
+++ app/src/main/java/com/androidchatapp/UserDetails.java
@@ -1,7 +1,16 @@
 package com.androidchatapp;
 
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
 public class UserDetails {
     static String username = "";
     static String password = "";
     static String chatWith = "";
+    static BufferedImage image;
 }
